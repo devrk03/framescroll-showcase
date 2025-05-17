f269c20da4174053d132e546144e2047c8f7db51
Raz <114949597+devrk03@users.noreply.github.com>
2025-05-17 20:44:39 +0530

    Create Next.config.js

---
+++ Next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/parallaxvision' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/parallaxvision' : '',
+}
+
+module.exports = nextConfig
